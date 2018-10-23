b363cd8fa72777b5d16e82d134d0faabc1802197
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 18:53:24 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word(sentence2)
   new_list = sentence1.split
   sentence_reversed = ""
-  new_list.each do |word|
+  new_list.collect do |word|
     sentence_reversed << "#{word.reverse!} " 
   end
 sentence_reversed.chop
